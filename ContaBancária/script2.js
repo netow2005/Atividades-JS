bd019f9cd3a014838804c9453ef488ac45de1ced
Antonio Rodrigues <netodnet@gmail.com>
2023-10-08 21:59:11 -0300

    Atualização de atividade!

--- ContaBancária/script2.js
+++ ContaBancária/script2.js
@@ -23,4 +23,50 @@ function verExtrato() {
     }
 }
 
+
+const ctx1 = document.getElementById("grafico1").getContext("2d");
+new Chart(ctx1, {
+    type: "bar",
+    data: {
+        labels: ["Categoria A", "Categoria B", "Categoria C"],
+        datasets: [{
+            label: "Gráfico de Barras 1",
+            data: [10, 20, 30],
+            backgroundColor: ["red", "green", "blue"],
+        }],
+    },
+});
+
+
+const ctx2 = document.getElementById("grafico2").getContext("2d");
+new Chart(ctx2, {
+    type: "pie",
+    data: {
+        labels: ["Categoria X", "Categoria Y", "Categoria Z"],
+        datasets: [{
+            data: [40, 30, 20],
+            backgroundColor: ["orange", "purple", "pink"],
+        }],
+    },
+});
+
+
+const ctx3 = document.getElementById("grafico3").getContext("2d");
+new Chart(ctx3, {
+    type: "line",
+    data: {
+        labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio"],
+        datasets: [{
+            label: "Gráfico de Linha 1",
+            data: [10, 20, 15, 30, 25],
+            borderColor: "blue",
+            fill: false,
+        }],
+    },
+});
+
+
+
+
+
 document.getElementById("extratoButton").addEventListener("click", verExtrato);
